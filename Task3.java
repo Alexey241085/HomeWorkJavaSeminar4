b7d3765496b7d99d6b7d023d260c66dd867f10e5
Alexey <besh1985@mail.ru>
2023-04-24 00:47:41 +0300

    best version

--- Task3.java
+++ Task3.java
@@ -6,64 +6,61 @@ public class Task3 {
         
         LinkedList <Integer> list = new LinkedList<>();
         try (Scanner sc = new Scanner(System.in)) {
-            double result = 0;
+        double result = 0;
             
-            print("Введите число: ");
-            double num1 = sc.nextInt();
-            print("Введите знак '*', '/' , '+', '-': ");
-            String simbol = sc.next();
-            print("Введите число: ");
+        print("Введите число: ");
+        double num1 = sc.nextInt();
+        print("Введите знак '*', '/' , '+', '-': ");
+        String simbol = sc.next();
+        print("Введите число: ");
             double num2 = sc.nextInt();
             
-            switch (simbol) {
-                case "+": result = num1 + num2; 
-                    break;
-                case "-": result = num1 - num2; 
-                    break;
-                case "/": result = num1 / num2; 
-                    break;
-                case "*": result = num1 * num2; 
-                    break;
-                default:
-                    break;
-            }
-            System.out.println("Ответ: "  + result);
-            list.add((int) result);
-            
-            while(true){
-                print("Введите знак '*', '/' , '+', '-': ");
-                simbol = sc.next();
-                print("Введите число: ");
-                num2 = sc.nextInt();
-
-                switch (simbol) {
-                    case "+": result = result + num2;
-                              list.add((int) result); 
-                        break;
-                    case "-": result = result - num2;
-                              list.add((int) result); 
-                        break;
-                    case "/": result = result / num2;
-                              list.add((int) result); 
-                        break;
-                    case "*": result = result * num2; 
-                                  list.add((int) result);
-                        break;
-                    case "отмена": 
-                        // System.out.println(list);
-                        list.removeLast();
-                        result = list.getLast();
-                        break;
-                    default:
-                        break;
-                    }
-                    System.out.println("Ответ: "  + result);
-
-                }
-            }  
+        switch (simbol) {
+            case "+": result = num1 + num2; 
+                break;
+            case "-": result = num1 - num2; 
+                break;
+            case "/": result = num1 / num2; 
+                break;
+            case "*": result = num1 * num2; 
+                break;
+            default:
+                break;
         }
-        
-        
+        System.out.println("Ответ: "  + result);
+        list.add((int) result);
+            
+        while(true){
+            print("Введите знак '*', '/' , '+', '-': ");
+            simbol = sc.next();
+            print("Введите число: ");
+            num2 = sc.nextInt();
+                
+        switch (simbol) {
+            case "+": result = result + num2;
+            list.add((int) result); 
+                break;
+            case "-": result = result - num2;
+            list.add((int) result); 
+                break;
+            case "/": result = result / num2;
+            list.add((int) result); 
+                break;
+            case "*": result = result * num2; 
+            list.add((int) result);
+                break;
+            case "отмена":     
+            list.removeLast();
+            result = list.getLast();
+                break;
+            default:
+                break;
+            }
+                System.out.println("Ответ: "  + result);
+
+            }
+        }  
+    }
         
         public static void print(String a){
             System.out.print(a);
